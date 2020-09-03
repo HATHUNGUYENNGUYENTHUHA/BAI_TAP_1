18513b482ceb38f8769ad1d262f7089fe33187de
thu ha <nguyenthuha110929@gmail.com>
2020-09-03 19:18:20 +0700

    thu git

---
+++ src/com/vn/git/GitClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.git;
+
+/**
+ *
+ * @author nguye
+ */
+public class GitClass {
+    public static void main(String[] args) {
+            System.out.println("thu git");
+    }  
+}
